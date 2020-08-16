c2055c5bf06ff4f46354f4f80ba33d19fe237151
Andrés Pérez <11398826+Oxyrus@users.noreply.github.com>
2020-08-15 19:47:57 -0500

    Add option to select txt file

--- Compiler/Form1.Designer.cs
+++ Compiler/Form1.Designer.cs
@@ -31,14 +31,21 @@
             this.codeTextBox = new System.Windows.Forms.TextBox();
             this.outputTextBox = new System.Windows.Forms.TextBox();
             this.compileButton = new System.Windows.Forms.Button();
+            this.optionsTabControl = new System.Windows.Forms.TabControl();
+            this.editorTab = new System.Windows.Forms.TabPage();
+            this.fileSelectTab = new System.Windows.Forms.TabPage();
+            this.selectFileButton = new System.Windows.Forms.Button();
+            this.optionsTabControl.SuspendLayout();
+            this.editorTab.SuspendLayout();
+            this.fileSelectTab.SuspendLayout();
             this.SuspendLayout();
             // 
             // codeTextBox
             // 
-            this.codeTextBox.Location = new System.Drawing.Point(12, 127);
+            this.codeTextBox.Location = new System.Drawing.Point(3, 3);
             this.codeTextBox.Multiline = true;
             this.codeTextBox.Name = "codeTextBox";
-            this.codeTextBox.Size = new System.Drawing.Size(343, 203);
+            this.codeTextBox.Size = new System.Drawing.Size(329, 196);
             this.codeTextBox.TabIndex = 0;
             // 
             // outputTextBox
@@ -60,16 +67,62 @@
             this.compileButton.UseVisualStyleBackColor = true;
             this.compileButton.Click += new System.EventHandler(this.compileButton_Click);
             // 
+            // optionsTabControl
+            // 
+            this.optionsTabControl.Controls.Add(this.editorTab);
+            this.optionsTabControl.Controls.Add(this.fileSelectTab);
+            this.optionsTabControl.Location = new System.Drawing.Point(12, 100);
+            this.optionsTabControl.Name = "optionsTabControl";
+            this.optionsTabControl.SelectedIndex = 0;
+            this.optionsTabControl.Size = new System.Drawing.Size(343, 230);
+            this.optionsTabControl.TabIndex = 3;
+            // 
+            // editorTab
+            // 
+            this.editorTab.Controls.Add(this.codeTextBox);
+            this.editorTab.Location = new System.Drawing.Point(4, 24);
+            this.editorTab.Name = "editorTab";
+            this.editorTab.Padding = new System.Windows.Forms.Padding(3);
+            this.editorTab.Size = new System.Drawing.Size(335, 202);
+            this.editorTab.TabIndex = 0;
+            this.editorTab.Text = "Editor";
+            this.editorTab.UseVisualStyleBackColor = true;
+            // 
+            // fileSelectTab
+            // 
+            this.fileSelectTab.Controls.Add(this.selectFileButton);
+            this.fileSelectTab.Location = new System.Drawing.Point(4, 24);
+            this.fileSelectTab.Name = "fileSelectTab";
+            this.fileSelectTab.Padding = new System.Windows.Forms.Padding(3);
+            this.fileSelectTab.Size = new System.Drawing.Size(335, 202);
+            this.fileSelectTab.TabIndex = 1;
+            this.fileSelectTab.Text = "File Select";
+            this.fileSelectTab.UseVisualStyleBackColor = true;
+            // 
+            // selectFileButton
+            // 
+            this.selectFileButton.Location = new System.Drawing.Point(120, 83);
+            this.selectFileButton.Name = "selectFileButton";
+            this.selectFileButton.Size = new System.Drawing.Size(75, 23);
+            this.selectFileButton.TabIndex = 1;
+            this.selectFileButton.Text = "Select file";
+            this.selectFileButton.UseVisualStyleBackColor = true;
+            this.selectFileButton.Click += new System.EventHandler(this.selectFileButton_Click);
+            // 
             // Compiler
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 389);
+            this.Controls.Add(this.optionsTabControl);
             this.Controls.Add(this.compileButton);
             this.Controls.Add(this.outputTextBox);
-            this.Controls.Add(this.codeTextBox);
             this.Name = "Compiler";
             this.Text = "Compiler";
+            this.optionsTabControl.ResumeLayout(false);
+            this.editorTab.ResumeLayout(false);
+            this.editorTab.PerformLayout();
+            this.fileSelectTab.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,6 +133,10 @@
         private System.Windows.Forms.TextBox codeTextBox;
         private System.Windows.Forms.TextBox outputTextBox;
         private System.Windows.Forms.Button compileButton;
+        private System.Windows.Forms.TabControl optionsTabControl;
+        private System.Windows.Forms.TabPage editorTab;
+        private System.Windows.Forms.TabPage fileSelectTab;
+        private System.Windows.Forms.Button selectFileButton;
     }
 }
 
--- Compiler/Form1.cs
+++ Compiler/Form1.cs
@@ -1,19 +1,54 @@
 ﻿using System;
+using System.IO;
+using System.Security;
 using System.Windows.Forms;
 
 namespace Compiler
 {
     public partial class Compiler : Form
     {
+        private OpenFileDialog _openFileDialog;
+        private string _fileText = string.Empty;
+
         public Compiler()
         {
             InitializeComponent();
+            _openFileDialog = new OpenFileDialog()
+            {
+                FileName = "Select a text file",
+                Filter = "Text files (*.txt)|*.txt",
+                Title = "Open text file"
+            };
         }
 
         private void compileButton_Click(object sender, EventArgs e)
         {
-            var output = new Output(codeTextBox.Text);
-            outputTextBox.Text = output.FormattedValue;
+            if (optionsTabControl.SelectedTab == editorTab)
+            {
+                var output = new Output(codeTextBox.Text);
+                outputTextBox.Text = output.FormattedValue;
+            }
+            else
+            {
+                var output = new Output(_fileText);
+                outputTextBox.Text = output.FormattedValue;
+            }
+        }
+
+        private void selectFileButton_Click(object sender, EventArgs e)
+        {
+            if (_openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var filePath = _openFileDialog.FileName;
+                    _fileText = File.ReadAllText(filePath);
+                }
+                catch (SecurityException)
+                {
+                    MessageBox.Show("Security error");
+                }
+            }
         }
     }
 }
